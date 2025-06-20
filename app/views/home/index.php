0e776540b05e7fbf92f8718dc567dc6d459219cc
Muhammad Abdullah Adil <abdullahadil143@gmail.com>
2025-06-20 23:45:42 +0000

    added css styling

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -11,7 +11,8 @@
 
     <div class="row">
         <div class="col-lg-12">
-            <p> <a href="/logout">Click here to logout</a></p>
+            <a href="/logout" class="logout-button">Logout</a>
+
         </div>
     </div>
 
